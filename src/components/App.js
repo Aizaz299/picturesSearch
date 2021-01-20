aae68b7c3d52baa31120f8fd9b4286b8ea8fa876
Aizaz Rehman <aziaz299@gmail.com>
2021-01-21 02:36:56 +0400

    getting data .then

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
+
 class App extends React.Component {
+
+  start={images:[]};
 onSearchSubmit(term){
-console.log(term)
+//console.log(term)
+axios.get('https://api.unsplash.com/search/photos',
+{
+  params:{query:term},
+headers:{
+Authorization:'Client-ID aSYI81rvCekBxkVA9jQ6QKr_qXmPw3x7jeNtvRzO8ME'
+}
+})
+.then(response=>{
+  console.log(response)
+});
+//this.setState({images: response.data.results});
 }
 
 
@@ -14,9 +29,10 @@ console.log(term)
             className="ui container" 
            style={{marginTop:'15px'}}>
            <SearchBar onSubmiT={this.onSearchSubmit}/> 
+           
      </div>
   )
-  }
+  } 
 
 };
 
